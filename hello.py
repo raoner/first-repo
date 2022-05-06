fac58e17f5ee09b76d365423f409bee6a533f541
raoner <suitable1125@gmail.com>
2022-05-06 13:24:04 +0900

    aq,aq2 complet!

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
+for i in range(1,11):
+	
+	if i % 5 == 0:
+		print('dream')
+	else:
+		print(i)
+
+for i in range(1,11):
+	if i % 15 == 0:
+		print('yeardream')
+	else:
+		print(i)
+
+
+
+
 print('hello')
