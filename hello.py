0f66fad4f416c9db3aa7c4f6bf046f9b7291b56b
raoner <suitable1125@gmail.com>
2022-05-06 13:35:05 +0900

    q, aq, aq2 completed!

--- hello.py
+++ hello.py
@@ -1,16 +1,12 @@
-for i in range(1,11):
-	
-	if i % 5 == 0:
-		print('dream')
-	else:
-		print(i)
-
-for i in range(1,11):
+for i in range(1, 21):
 	if i % 15 == 0:
 		print('yeardream')
+	elif i % 3 == 0:
+		print('dream')
+	elif i % 5 == 0:
+		print('year')
 	else:
 		print(i)
-
 
 
 
